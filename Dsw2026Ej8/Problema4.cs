afcb1c5c1115ada7d41e76ebe98bd1653ff8a760
MarianoRuiz <marianoruizcoronel@gmail.com>
2026-05-02 19:27:44 -0300

    Add Punto 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8;
+
+static internal class Problema4
+{
+    static public double CalcularPromedio(int? nota1,int? nota2,int? nota3)
+    {
+        return (double) (( (nota1 > 0.0) && (nota1 < 10.0) ? nota1 : 0.0) + (nota2 > 0.0 && nota2 < 10.0 ? nota2 : 0.0) + (nota3 > 0.0 && nota3 < 10.0 ? nota3 : 0.0)) / 3.0;
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Problema2.CrearResumenVenta(123, "Producto A", 5, 10.5m));
+        // Console.WriteLine(Problema2.CrearResumenVenta(123, "Producto A", 5, 10.5m));
+        Console.WriteLine(Problema4.CalcularPromedio(null, null, null));
     }
 }
