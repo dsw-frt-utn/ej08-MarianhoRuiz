70a027c547162fb5b2ab7599a9677882dc09cb01
MarianoRuiz <marianoruizcoronel@gmail.com>
2026-05-02 21:53:03 -0300

    Add: Punto 5

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Dsw2026Ej8;
+
+public class Problema5
+{
+    public class Sale
+    {
+        private decimal amount;
+        private decimal discount;
+
+        public Sale(decimal amount, decimal discount)
+        {
+            this.amount = amount;
+            this.discount = discount;
+        }
+
+        public decimal Amount { get => amount; set => amount = value; }
+        public decimal Discount { get => discount; set => discount = value; }
+
+        /* public decimal GetAmount()
+         {
+             return Amount;
+         }
+         public decimal GetDiscount()
+         {
+             return Discount;
+         }
+         public void setAmount(decimal amount)
+         {
+             Amount = amount;
+         }
+         public void setDiscount(decimal discount)
+         {
+             Discount = discount;
+         }
+         */
+
+
+        public virtual decimal CalculateTotal()
+        {
+            return Amount - Discount;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public RetailSale(decimal amount, decimal discount) : base(amount, discount)
+        {
+        }
+
+        public override decimal CalculateTotal()
+        {
+            return this.Amount;
+        }
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal amount, decimal discount) : base(amount, discount)
+        {
+        }
+
+        public override decimal CalculateTotal()
+        {
+            return this.Amount * 0.90m; 
+        }
+    }
+
+
+
+    public decimal ObtenerImporteFinal(Sale sale)
+    { 
+        var total = sale.CalculateTotal();
+        return total;
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,6 +5,9 @@ internal class Program
     static void Main(string[] args)
     {
         // Console.WriteLine(Problema2.CrearResumenVenta(123, "Producto A", 5, 10.5m));
-        Console.WriteLine(Problema4.CalcularPromedio(null, null, null));
+        // Console.WriteLine(Problema4.CalcularPromedio(null, null, null));
+        var aux = new Problema5.WholesaleSale(2000.0m, 100.0m);
+        Console.WriteLine(aux.CalculateTotal());
+
     }
 }
