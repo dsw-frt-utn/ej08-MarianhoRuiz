1611f7c64dcd570f540bc6b2f1134cc156ce0e78
MarianoRuiz <marianoruizcoronel@gmail.com>
2026-05-02 22:15:23 -0300

    Add: Punto 6

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -6,8 +6,10 @@ internal class Program
     {
         // Console.WriteLine(Problema2.CrearResumenVenta(123, "Producto A", 5, 10.5m));
         // Console.WriteLine(Problema4.CalcularPromedio(null, null, null));
-        var aux = new Problema5.WholesaleSale(2000.0m, 100.0m);
-        Console.WriteLine(aux.CalculateTotal());
+        // var aux = new Problema5.WholesaleSale(2000.0m, 100.0m);
+        // Console.WriteLine(aux.CalculateTotal());
+        // Console.WriteLine(Problema6.NormalizarCodigoProducto(null));
 
+        // para las pruebas se tomo la decision de hacer las clases necesarias static porque sino era muy engorroso
     }
 }
